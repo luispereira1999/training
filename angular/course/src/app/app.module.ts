32717cfd15375fb6b09137e45bc5e123ec3330eb
luispereira1999 <67525203+luispereira1999@users.noreply.github.com>
2020-12-11 11:15:15 +0000

    Interpolação & Property binding

--- angular/course/src/app/app.module.ts
+++ angular/course/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { CoursesModule } from './courses/courses.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { DataBindingComponent } from './data-binding/data-binding.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    DataBindingComponent
   ],
   imports: [
     BrowserModule,
---
+++ angular/course/src/app/data-binding/data-binding.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-data-binding',
+  templateUrl: './data-binding.component.html',
+  styleUrls: ['./data-binding.component.less']
+})
+export class DataBindingComponent implements OnInit {
+  url: string = "https://angular.io/";
+  imageUrl: string = "https://images.unsplash.com/photo-1607579771747-1aa92c36515c?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=334&q=80";
+  title: string = "Hello World"
+
+  getValue() {
+    return 15;
+  }
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
