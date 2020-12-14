98f8f62099e1339651684b7162b7bc7a806f646a
luispereira1999 <67525203+luispereira1999@users.noreply.github.com>
2020-12-14 10:48:50 +0000

    Event binding

--- angular/course/src/app/data-binding/data-binding.component.ts
+++ angular/course/src/app/data-binding/data-binding.component.ts
@@ -7,12 +7,27 @@ import { Component, OnInit } from '@angular/core';
 })
 
 export class DataBindingComponent {
+
   url: string = "https://angular.io/";
   imageUrl: string = "https://images.unsplash.com/photo-1607579771747-1aa92c36515c?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=334&q=80";
   title: string = "Hello World"
+  writtenText: string = "";
+  savedText: string = "";
 
   getValue() {
     return 15;
+  }
+
+  buttonClicked() {
+    alert("Botão clicado!");
+  }
+
+  onKeyUp(event: KeyboardEvent) {
+    this.writtenText = (<HTMLInputElement>event.target).value;
+  }
+
+  saveText(text: string) {
+    this.savedText = text;
   }
 
   constructor() { }
